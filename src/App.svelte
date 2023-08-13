1eb737e68e6f815b7d564840262ac6a338883c3e
James Myose <jmyose@live.com>
2023-08-13 01:17:48 -0500

    feat: trying to get routes working

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
-  import { Router, Route, links } from 'svelte-routing';
+  import { Router, Route, Link, links } from 'svelte-routing';
 
   import CurrentTournament from './pages/CurrentTournament.svelte';
   import Home from './pages/Home.svelte';
+  import U1600Final from './pages/U1600Final.svelte'
 
   export let url = '/';
 </script>
 
 <Router {url}>
-  <div use:links>
+  <nav use:links>
+    <Link to="/">Home</Link>
+    <Link to="/currentTournament.html">CurrentTournament</Link>
+    <Link to="/1600final.html">U1600Final</Link>
+  </nav>
+  <div>
     <Route path='/' component={Home}/>
     <Route path='/currentTournament.html' component={CurrentTournament}/>
-    <Route path='/1600final.html' component={CurrentTournament}/>
-    <Route path='/aboutUs.html' component={CurrentTournament}/>
+    <Route path='/1600final.html' component={U1600Final}/>
+    <!-- <Route path='/aboutUs.html' component={CurrentTournament}/>
     <Route path='/clubPairings.html' component={CurrentTournament}/>
     <Route path='/clubStandings.html' component={CurrentTournament}/>
     <Route path='/donors.html' component={CurrentTournament}/>
@@ -24,6 +30,6 @@
     <Route path='/template.html' component={CurrentTournament}/>
     <Route path='/tournamentHistory.html' component={CurrentTournament}/>
     <Route path='/upcomingTournaments.html' component={CurrentTournament}/>
-    <Route path='/whatINeed.html' component={CurrentTournament}/>
+    <Route path='/whatINeed.html' component={CurrentTournament}/> -->
   </div>
 </Router>
