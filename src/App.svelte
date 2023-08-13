bbac717b2e0aa79e5de3314bae16c6bae98504a2
James Myose <jmyose@live.com>
2023-08-12 22:19:54 -0500

    feat: implement router for usage with a tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+  import { Router, Link, Route, links } from 'svelte-routing';
+
   import CurrentTournament from './pages/CurrentTournament.svelte';
-  import Home from './pages/Home.svelte'
+  import Home from './pages/Home.svelte';
+
+  export let url = '/';
 </script>
 
-<!-- <Home /> -->
-<CurrentTournament />
+<Router {url}>
+  <div use:links>
+    <Route path='/' component={Home}/>
+    <Route path='/currentTournament.html' component={CurrentTournament}/>
+  </div>
+</Router>
