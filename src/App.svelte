5d51d1cf4b4c2bacfa77831eb6bd5e5fa78d89cb
James Myose <jmyose@live.com>
2023-08-12 22:53:44 -0500

    feat: create routes for pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Router, Link, Route, links } from 'svelte-routing';
+  import { Router, Route, links } from 'svelte-routing';
 
   import CurrentTournament from './pages/CurrentTournament.svelte';
   import Home from './pages/Home.svelte';
@@ -11,5 +11,19 @@
   <div use:links>
     <Route path='/' component={Home}/>
     <Route path='/currentTournament.html' component={CurrentTournament}/>
+    <Route path='/1600final.html' component={CurrentTournament}/>
+    <Route path='/AboutUs.html' component={CurrentTournament}/>
+    <Route path='/ClubPairings.html' component={CurrentTournament}/>
+    <Route path='/ClubStandings.html' component={CurrentTournament}/>
+    <Route path='/Donors.html' component={CurrentTournament}/>
+    <Route path='/EloPerfCalc.html' component={CurrentTournament}/>
+    <Route path='/Lessons.html' component={CurrentTournament}/>
+    <Route path='/OpenFinal.html' component={CurrentTournament}/>
+    <Route path='/RunningATournament.html' component={CurrentTournament}/>
+    <Route path='/SeniorFinal.html' component={CurrentTournament}/>
+    <Route path='/Template.html' component={CurrentTournament}/>
+    <Route path='/TournamentHistory.html' component={CurrentTournament}/>
+    <Route path='/UpcomingTournaments.html' component={CurrentTournament}/>
+    <Route path='/WhatINeed.html' component={CurrentTournament}/>
   </div>
 </Router>
