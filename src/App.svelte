7a910deb0fc83425ec08f6710581d9bd3fa5764a
James Myose <jmyose@live.com>
2023-08-13 01:54:09 -0500

    chore: remove unnecessary imports and console log

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-
   import routes from './routes';
   import Router from 'svelte-spa-router'
-
-  import CurrentTournament from './pages/CurrentTournament.svelte';
-  import Home from './pages/Home.svelte';
-  import U1600Final from './pages/U1600Final.svelte'
-
-  console.log(routes);
 </script>
 
 <Router {routes} />
