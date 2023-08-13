5a914be81f477f5144dfafc5eed4dae46c44a7cb
James Myose <jmyose@live.com>
2023-08-12 21:40:48 -0500

    feat: convert current tournament page

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import CurrentTournament from './pages/CurrentTournament.svelte';
   import Home from './pages/Home.svelte'
 </script>
 
-<Home />
+<!-- <Home /> -->
+<CurrentTournament />
