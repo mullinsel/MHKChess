75379ee51f9c0cf529268dfcdab6f81f19f096e2
James Myose <jmyose@live.com>
2023-08-12 21:24:29 -0500

    chore: delete unnecessary imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import Home from './pages/Home.svelte'
 </script>
 
-<main>
-  <Home />
-</main>
+<Home />
