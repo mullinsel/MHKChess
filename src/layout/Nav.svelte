76e7e4870ae1ddaa0ab9403fb6c65dfe43e1cb53
James Myose <jmyose@live.com>
2023-09-19 20:13:38 -0500

    create reusable header and nav components

---
+++ src/layout/Nav.svelte
@@ -0,0 +1,33 @@
+<div>
+    <button><a href="#/currentTournament" class="button">Current Tournament</a></button>
+    <button><a href="#/upcomingTournaments"class="button" style="display:inline;">Upcoming Tournaments</a></button>
+    <button><a href="#/tournamentHistory"class="button">Tournament History</a></button>
+    <button><a href="#/aboutUs" class="button">About Us</a></button>
+</div>
+
+<style>
+    a.button {
+        -webkit-appearance: button;
+        -moz-appearance: button;
+        appearance: button;
+        height: 10px;
+        width: 200px;
+        font-size: 18px;
+        text-decoration: none;
+        color: initial;
+    }
+    a:link {
+            color: black;
+            background-color: transparent;
+            text-decoration: underline;
+    }
+    a:visited {
+            color: black;
+            background-color: transparent;
+            text-decoration: underline;
+    }
+    a:active {
+            color: black;
+            background-color: transparent;
+    }
+</style>
