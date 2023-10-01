e57eeff1251e99badde3cab47bb48ac73ac58cc2
James Myose <jmyose@live.com>
2023-09-30 20:32:24 -0500

    create new home page

---
+++ src/pages/OldHome.svelte
@@ -0,0 +1,93 @@
+<script lang='ts'>
+    import Footer from "../layout/Footer.svelte";
+</script>
+
+<div class="home">
+    <div class = "Header">
+        <img src="images/logo.png" alt="Club logo" style = "position:absolute; top:4px; left:30px;" width="225" height="190"/>
+        <img src="images/white.png" alt="Header" width="980" height="200" style = "border: 1px solid black;"/>
+        <p style="color:black; position:absolute; top:-50px; left:275px; font-size:80px;">MHK Chess Club</p>
+        <a target="_blank" href ="https://www.chess.com/clubs/members/manhattan-chess"> <img src="images/chess.png" style="position:absolute; top:150px; left:880px" alt="Chess Logo" width="100" height="40"/></a>
+        <a target="_blank" href ="https://www.flickr.com/photos/198250331@N07/"> <img src="images/flickerlogo.png" style="position:absolute; top:100px; left:900px" alt="Flicker Logo" width="80" height="40"/></a>
+        <a target="_blank" href = "https://www.facebook.com/Manhattanhessclub"><img src="images/fblogo.png" style="position:absolute; top:50px; left:940px" alt="Facebook Logo" width="40" height="40"/></a>
+        <p style="color:black; position:absolute; top:100px; left:275px; font-size:30px;">Thursdays and Sundays 5pm</p>
+        <p style="color:black; position:absolute; top:130px; left:275px; font-size:30px;">at UFM Community Learning Center</p>
+        <!--<p style="color:black; position:absolute; top:130px; left:325px; font-size:30px;">Thursday 6pm-9pm and Sunday 6pm-9pm</p>--->
+    </div>
+    <div class = "Navigation bar">
+        <button><a href="#/currentTournament" class="button">Current Tournament</a></button>
+        <button><a href="#/upcomingTournaments"class="button" style="display:inline;">Upcoming Tournaments</a></button>
+        <button><a href="#/tournamentHistory"class="button">Tournament History</a></button>
+        <button><a href="#/aboutUs" class="button">About Us</a></button>
+    </div>
+    <br>
+    <br>
+    <br>
+    <button><a href="#/whatINeed" class="button">What do I need to play?</a></button>
+    <br>
+    <button><a href="#/elo-perf-calc" class="button">Rating Calculator</a></button>
+    <br>
+    <br>
+    <br>
+    <button><a href="https://tryufm.org/"class="button">UFM Community Learning Center</a></button>
+    <button><a href="http://www.kansaschess.org/"class="button">Kansas Chess Association</a></button>
+    <br>
+    <br>
+    <button><a href="#/donors" class="button">Donors List</a></button>
+    <br>
+    <button><a href="https://www.paypal.com/donate/?hosted_button_id=N7BF4U2LKREKU&fbclid=IwAR335J2n6SALXAnVYFxivOOypj9-9tUcr5jPDWT-Rim7m26bN8yLFczjsTM" class="button">Donate to MHK Chess</a></button>
+    <br>
+    <br>
+    <br>
+    <h3 style="color:black">Join our Email list</h3>
+    <form method="POST" action="https://script.google.com/macros/s/AKfycbztywQ8k-papG011ZLe7mO5iQoRRh-fhkuzokZw6rnLK_K6j6MsKrKJymtteusrpPs/exec">
+        <input name="Email" type="email" placeholder="Email" required>
+        <input name="Name" type="text" placeholder="Name" required>
+        <button type="submit">Join</button>
+    </form>
+    <br>
+    <br>
+    <Footer />
+</div>
+
+<style>
+    a.button {
+        -webkit-appearance: button;
+        -moz-appearance: button;
+        appearance: button;
+        height: 10px;
+        width: 200px;
+        font-size: 18px;
+        text-decoration: none;
+        color: initial;
+    }
+    .home {
+        text-align: center;
+        background-color: LightBlue;
+        color: white;
+    }
+    .home {
+       height:620px;
+       width:1023px;
+       position:absolute;
+       box-sizing:border-box;
+       transform-origin: 0 0;
+       -moz-transform-origin:0 0;
+       -o-transform-origin: 0 0;
+       -webkit-transform-origin: 0 0;
+    }
+    a:link {
+            color: black;
+            background-color: transparent;
+            text-decoration: underline;
+    }
+    a:visited {
+            color: black;
+            background-color: transparent;
+            text-decoration: underline;
+    }
+    a:active {
+            color: black;
+            background-color: transparent;
+    }
+</style>
