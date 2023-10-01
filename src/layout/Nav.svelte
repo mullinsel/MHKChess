b745131e8f426f83f1f97c897bfaa9e3199b05da
James Myose <jmyose@live.com>
2023-10-01 00:08:59 -0500

    remove button class

--- src/layout/Nav.svelte
+++ src/layout/Nav.svelte
@@ -7,12 +7,12 @@
             <TempLogo/>
         </a>
         <ul class="list-none ml-auto flex flex-row gap-6">
-            <li><a href="#/currentTournament" class="button">Current Tournament</a></li>
+            <li><a href="#/currentTournament">Current Tournament</a></li>
             <li>
-                <a href="#/upcomingTournaments" class="button">Upcoming Tournaments</a>
+                <a href="#/upcomingTournaments">Upcoming Tournaments</a>
             </li>
-            <li><a href="#/tournamentHistory" class="button">Tournament History</a></li>
-            <li><a href="#/aboutUs" class="button">About Us</a></li>
+            <li><a href="#/tournamentHistory">Tournament History</a></li>
+            <li><a href="#/aboutUs">About Us</a></li>
         </ul>
     </div>
 </nav>
