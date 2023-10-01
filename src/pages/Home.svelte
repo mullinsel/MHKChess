c4de5460bef71ab7ce5defff7d5d74176aca2dcc
James Myose <jmyose@live.com>
2023-09-30 23:59:24 -0500

    add background styling

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,4 +2,4 @@
     import Nav from '../layout/Nav.svelte';
 </script>
 
-<div class="min-h-screen h-full"><Nav /></div>
+<div class="min-h-screen h-full bg-stone-100 dark:bg-gray-900 dark:text-white"><Nav /></div>
