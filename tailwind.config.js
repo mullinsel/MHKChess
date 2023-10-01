829bdee90f4c370a5757a3decb5328a8961e1e4f
James Myose <jmyose@live.com>
2023-09-30 23:17:35 -0500

    define them for font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.svelte'],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.svelte"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    fontSize: {
+      sm: "0.707rem",
+      base: "1rem",
+      xl: "1.414rem",
+      "2xl": "1.999rem",
+      "3xl": "2.827rem",
+      "4xl": "3.997rem",
+      "5xl": "5.652rem",
     },
-    plugins: [],
+    fontFamily: {
+      heading: "Tiro Devanagari Marathi",
+      body: "Tiro Devanagari Marathi",
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
+  },
 };
-
