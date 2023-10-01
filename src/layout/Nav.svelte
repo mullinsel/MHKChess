ecd27d4437a89257a97f8ff7ee0657d376446cff
James Myose <jmyose@live.com>
2023-09-30 23:17:49 -0500

    add nav component

--- src/layout/Nav.svelte
+++ src/layout/Nav.svelte
@@ -1,39 +1,18 @@
-<nav>
-    <ul class="list-none m-0">
-        <li><a href="#/currentTournament" class="button">Current Tournament</a></li>
-        <li>
-            <a href="#/upcomingTournaments" class="button" style="display:inline;"
-                >Upcoming Tournaments</a
-            >
-        </li>
-        <li><a href="#/tournamentHistory" class="button">Tournament History</a></li>
-        <li><a href="#/aboutUs" class="button">About Us</a></li>
-    </ul>
+<script>
+    import TempLogo from './TempLogo.svelte'
+</script>
+<nav class="shadow-md h-20">
+    <div class="pl-6 pr-6 flex flex-row items-center h-full">
+        <a href="/" class="button">
+            <TempLogo/>
+        </a>
+        <ul class="list-none ml-auto flex flex-row gap-6">
+            <li><a href="#/currentTournament" class="button">Current Tournament</a></li>
+            <li>
+                <a href="#/upcomingTournaments" class="button">Upcoming Tournaments</a>
+            </li>
+            <li><a href="#/tournamentHistory" class="button">Tournament History</a></li>
+            <li><a href="#/aboutUs" class="button">About Us</a></li>
+        </ul>
+    </div>
 </nav>
-
-<style>
-    a.button {
-        -webkit-appearance: button;
-        -moz-appearance: button;
-        appearance: button;
-        height: 10px;
-        width: 200px;
-        font-size: 18px;
-        text-decoration: none;
-        color: initial;
-    }
-    a:link {
-        color: black;
-        background-color: transparent;
-        text-decoration: underline;
-    }
-    a:visited {
-        color: black;
-        background-color: transparent;
-        text-decoration: underline;
-    }
-    a:active {
-        color: black;
-        background-color: transparent;
-    }
-</style>
