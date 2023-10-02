9730cff40cca6fc0ec07ef604de8812d5e63e397
James Myose <jmyose@live.com>
2023-10-01 21:57:35 -0500

    make nav work correctly

--- src/layout/Nav.svelte
+++ src/layout/Nav.svelte
@@ -2,17 +2,20 @@
     import TempLogo from './TempLogo.svelte'
 </script>
 <nav class="shadow-md h-20 bg-stone-100 dark:bg-slate-800 dark:text-white">
-    <div class="pl-6 pr-6 flex flex-row items-center h-full">
-        <a href="/" class="button">
+    <div class="pl-6 pr-6 grid grid-rows-1 grid-cols-[200px_auto_200px] h-full items-center">
+        <a href="/" class="">
             <TempLogo/>
         </a>
-        <ul class="list-none ml-auto flex flex-row gap-6">
-            <li><a href="#/currentTournament">Current Tournament</a></li>
-            <li>
-                <a href="#/upcomingTournaments">Upcoming Tournaments</a>
-            </li>
-            <li><a href="#/tournamentHistory">Tournament History</a></li>
-            <li><a href="#/aboutUs">About Us</a></li>
-        </ul>
+        <div class="justify-self-center flex flex-row">
+            <ul class="list-none flex flex-row gap-6">
+                <li><a href="#/currentTournament">Current Tournament</a></li>
+                <li>
+                    <a href="#/upcomingTournaments">Upcoming Tournaments</a>
+                </li>
+                <li><a href="#/tournamentHistory">Tournament History</a></li>
+                <li><a href="#/aboutUs">About Us</a></li>
+            </ul>
+        </div>
+        <img class="h-9 justify-self-end" src="images/Facebook_Logo_Primary.png">
     </div>
 </nav>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,4 +2,16 @@
     import Nav from '../layout/Nav.svelte';
 </script>
 
-<div class="min-h-screen h-full bg-stone-100 dark:bg-gray-900 dark:text-white"><Nav /></div>
+<div class="h-screen bg-stone-100 dark:bg-gray-900 dark:text-white flex flex-col">
+    <Nav />
+    <div class="md:h-full grid md:grid-cols-2 md:gap-6">
+        <h1 class="p-6 font-heading text-4xl font-black m-auto">
+            The Manhattan Chess Club
+        </h1>
+        <div class="flex-col flex justify-center font-body p-6">
+            <p class="p-6 bg-amber-300 shadow-2xl rounded-lg h-3/4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut temporibus vero quae libero hic, quia vitae exercitationem eum perspiciatis. Molestias, aperiam repudiandae autem voluptatem atque tempora repellat ut incidunt distinctio.
+            </p>
+        </div>
+    </div>
+</div>
