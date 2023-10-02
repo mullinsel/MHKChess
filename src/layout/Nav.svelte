5033dfa343c547bc755ce6dd084b5ba4de15229d
James Myose <jmyose@live.com>
2023-10-01 23:23:41 -0500

    add comments

--- src/layout/Nav.svelte
+++ src/layout/Nav.svelte
@@ -2,6 +2,7 @@
     import TempLogo from './TempLogo.svelte'
     import HamburgerSvg from './HamburgerSvg.svelte'
 
+    // Whether to show mobile hamburger menu or not
     let showMobileNav = false;
     function onHamburgerClick() {
         showMobileNav = !showMobileNav;
@@ -10,9 +11,11 @@
 
 <nav class="h-20 bg-stone-100 dark:bg-slate-800 dark:text-white">
     <div class="pl-6 pr-6 grid grid-rows-1 grid-cols-[10rem_auto_10rem] h-full items-center">
+        <!-- Main Logo -->
         <a href="/" class="h-8 w-8">
             <TempLogo/>
         </a>
+        <!-- Other Pages -->
         <div class="hidden md:flex justify-self-center flex-row">
             <ul class="list-none flex flex-row gap-6">
                 <li><a href="#/currentTournament">Current Tournament</a></li>
@@ -23,9 +26,11 @@
                 <li><a href="#/aboutUs">About Us</a></li>
             </ul>
         </div>
+        <!-- Hamburger menu shows when mobile -->
         <button on:click={onHamburgerClick} class="justify-self-center md:hidden h-8 w-8">
             <HamburgerSvg/>
         </button>
+        <!-- Links to other sites -->
         <div class="justify-self-end flex flex-row gap-3">
             <a href="https://www.chess.com/clubs/members/manhattan-chess" target="_blank" class="h-8 w-7">
                 <img alt="chesscom_logo" class="h-8 m-auto" src="images/chesscom_pawn.png">
