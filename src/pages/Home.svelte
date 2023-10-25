f5e28784dee302f69a02b43de2cc0fe0a0c6d399
James Myose <jmyose@live.com>
2023-10-25 00:00:21 -0500

    adjust width of nav, add shadow

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="h-screen bg-stone-100 flex flex-col">
-    <div class="h-full sm:w-[640px] md:w-[768px] lg:w-[1024px] grid grid-rows-[5rem_1fr] m-auto">
+    <div class="h-full w-full grid grid-rows-[5rem_1fr]">
         <Nav />
-        <div class="h-full flex flex-col justify-center p-12">
+        <div class="h-full sm:w-[640px] md:w-[768px] lg:w-[1024px] flex flex-col justify-center p-12 m-auto">
             <h1 class="font-heading text-4xl font-black">
                 The Manhattan Chess Club
             </h1>
